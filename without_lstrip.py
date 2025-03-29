f20d6ef7a8bafbf5aeec0231e971d4e95286146e
Lou <marilounacional0717@gmail.com>
2025-03-29 23:14:45 +0800

    replaced

--- without_lstrip.py
+++ without_lstrip.py
@@ -1,3 +1,6 @@
 # ask user's input to add spaces in the beginning
+inputs = str(input("Enter your input and add spaces in the beginning: "))
 # remove spaces without using lstrip
+inputs = inputs.replace(" ", "")
 # print output
+print(inputs)
