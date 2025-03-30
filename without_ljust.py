5b514227fd8a5d9139825fc71f8ccd80e20eeca9
Lou <marilounacional0717@gmail.com>
2025-03-30 18:06:36 +0800

    prints with spaces

--- without_ljust.py
+++ without_ljust.py
@@ -1,3 +1,10 @@
 # ask user for input
+user = input("Enter your name: ")
+width = int(input("Enter the total width: "))
+
 # add spaces to the end of the name to match the specified width
+if len(user) < width:
+    user += " " * (width - len(user))
+
 # print output
+print(user, "'")
