a6ddc226c319231fd3984b3bc1f43defd1ead6b7
Lou <marilounacional0717@gmail.com>
2025-03-30 13:57:03 +0800

    print in lowercase

--- without_lower.py
+++ without_lower.py
@@ -1,3 +1,11 @@
 # ask user for input
+name = input("Enter your name: ")
 # manually convert the input to lowercase
+lower_case = ""
+for char in name:
+    if "A" <= char <= "Z":
+        lower_case += chr(ord(char) + 32)  # Convert uppercase to lowercase
+    else:
+        lower_case += char  # Keep other characters unchanged
 # print output
+print(lower_case)
