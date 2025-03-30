919894bba67217edbafa2cbc8f0cc4918fac69d8
Lou <marilounacional0717@gmail.com>
2025-03-30 18:49:11 +0800

    print in title case

--- without_title.py
+++ without_title.py
@@ -1,3 +1,6 @@
 # ask user for input
+user = input("Enter your name: ")
 # manually convert to title case 
+title = " ".join(word[0].upper() + word[1:].lower() if word else "" for word in user.split())
 # print output
+print(title)
