21c4f8c6edc5f80052b9e093662a58503fe9f592
Lou <marilounacional0717@gmail.com>
2025-03-30 18:37:40 +0800

    print swapcase

--- without_swapcase.py
+++ without_swapcase.py
@@ -1,3 +1,13 @@
 # ask user for input
+user = input("Enter your name: ")
 # reverse the casing of each character
+swapped_case = ""
+for char in user:
+    if "a" <= char <= "z":
+        swapped_case += chr(ord(char) - 32)  # convert lowercase to uppercase
+    elif "A" <= char <= "Z":
+        swapped_case += chr(ord(char) + 32)  # convert uppercase to lowercase
+    else:
+        swapped_case += char  # unchanged
 # print output
+print(swapped_case)
