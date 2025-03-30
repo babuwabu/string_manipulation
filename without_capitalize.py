d9df311b66a21ab03247c4421a57442f2e724fcd
Lou <marilounacional0717@gmail.com>
2025-03-30 18:43:58 +0800

    print capital

--- without_capitalize.py
+++ without_capitalize.py
@@ -1,3 +1,10 @@
 # ask user for input
+user = input("Enter your name: ")
 # manually capitalize the first letter 
+if user:
+    capitalized = user[0].upper() + user[1:].lower()
+else:
+    capitalized = ""
+
 # print output
+print(capitalized)
