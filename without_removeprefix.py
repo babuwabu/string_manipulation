dccb4804393dfe2e978f45c0bfba632fe232c28f
Lou <marilounacional0717@gmail.com>
2025-03-30 11:25:30 +0800

    removed without the use of removeprefix

--- without_removeprefix.py
+++ without_removeprefix.py
@@ -1,4 +1,9 @@
 # ask user for name
+name = input("Enter your name: ")
 # define the prefix to remove
+prefix_remove = input("Enter the prefix to remove: ")
 # remove the prefix if it exists at the beginning of the string
+if name.startswith(prefix_remove):
+    name = name.replace(prefix_remove, "", 1)
 # print output
+print(name)
