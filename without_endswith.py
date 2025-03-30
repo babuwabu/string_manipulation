4f55129027532825fcc532aa45be171dc80ff13d
Lou <marilounacional0717@gmail.com>
2025-03-30 17:47:59 +0800

    check and print

--- without_endswith.py
+++ without_endswith.py
@@ -1,3 +1,8 @@
 # ask user for input
-# check if the input ends with the given suffix manually
-# print output
+user = input("Enter your name: ")
+suffix = input("Enter the suffix to check: ")
+# check if the input ends with the given suffix manually and print output
+if len(user) >= len(suffix) and user[-len(suffix):] == suffix:
+    print("True")
+else:
+    print("False")
