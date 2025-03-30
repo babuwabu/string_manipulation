2013f032ccce7001871703fcc2c8e6d04129cd27
Lou <marilounacional0717@gmail.com>
2025-03-30 18:24:57 +0800

    print user in center with spaces

--- without_center.py
+++ without_center.py
@@ -1,3 +1,13 @@
 # ask user for input
+user = input("Enter your name: ")
+width = int(input("Enter the total width: "))
+
 # check left and right padding
+padding = max(0, width - len(user))
+left_padding = padding // 2
+right_padding = padding - left_padding
+
+total_space = " " * left_padding + user + " " * right_padding
+
 # print the output
+print(f'"{total_space}"')
