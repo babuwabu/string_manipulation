cdde85d02daa10f5fb8586ba6c17b533ad9e66fd
Lou <marilounacional0717@gmail.com>
2025-03-30 17:36:07 +0800

    print result

--- without_isupper.py
+++ without_isupper.py
@@ -1,3 +1,11 @@
 # ask user for input
+user = input("Enter your name: ")
 # check if all characters are uppercase
+all_upper = True
+
+for char in user:
+    if "a" <= char <= "z":  # checks if there are any lowercase letter found
+        all_upper = False
+        break
 # print the result
+print(all_upper)
