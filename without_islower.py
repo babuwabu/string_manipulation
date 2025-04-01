8c087eee4f8b82568d2083dded10a5c2c9a793a0
Lou <marilounacional0717@gmail.com>
2025-04-01 16:42:12 +0800

    print true or false

--- without_islower.py
+++ without_islower.py
@@ -1,3 +1,11 @@
 # ask user for input
+user = input("Enter your name: ")
 # manually check if all characters are lowercase 
+is_lower = True
+
+for char in user:
+    if "A" <= char <= "Z": 
+        is_lower = False
+        break
 # print the output
+print(is_lower)
