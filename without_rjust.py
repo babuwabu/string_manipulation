6a9daaef97cb5956a496bb7ddb36e28376e68133
Lou <marilounacional0717@gmail.com>
2025-04-01 16:56:05 +0800

    print output with width

--- without_rjust.py
+++ without_rjust.py
@@ -1,3 +1,8 @@
 # ask user for input
+user = input("Enter your name: ")
+width = int(input("Enter the total width: "))
 # add spaces to the beginning of the name to match the specified width
+if len(user) < width:
+    user = " " * (width - len(user)) + user
 # print the output
+print(repr(user))
