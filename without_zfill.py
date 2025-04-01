898ab8cd5712394d5109a76deefa17434492170f
Lou <marilounacional0717@gmail.com>
2025-04-01 17:02:30 +0800

    print with zeroes

--- without_zfill.py
+++ without_zfill.py
@@ -1,3 +1,8 @@
 # ask user for input
+user = input("Enter your name: ")
+width = int(input("Enter the total width: "))
 # add zeros to the beginning of the string to match the specified width
+if len(user) < width:
+    user = "0" * (width - len(user)) + user
 # print the output
+print(repr(user))
