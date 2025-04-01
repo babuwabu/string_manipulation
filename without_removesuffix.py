bcc50062cf26c395148cadc6ef6826b46f6b574c
Lou <marilounacional0717@gmail.com>
2025-04-01 16:20:53 +0800

    print no suffix

--- without_removesuffix.py
+++ without_removesuffix.py
@@ -1,3 +1,8 @@
 # ask user for input
+user = input("Enter your name: ")
+suffix = input("Enter the suffix to remove: ")
 # manually remove the suffix
+if user.endswith(suffix):
+    user = user[:-len(suffix)]
 # print output
+print(user)
