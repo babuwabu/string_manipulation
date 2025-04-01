bfc98fcb4d3ec99f48509f7bad3c98c2185b3efe
Lou <marilounacional0717@gmail.com>
2025-04-01 17:26:12 +0800

    print last index of char

--- without_rindex.py
+++ without_rindex.py
@@ -1,3 +1,14 @@
 # ask user for input
+user = input("Enter text: ")
+char_find = input("Enter the character to find: ")
 # find the last occurrence manually
+index = -1
+for i in range(len(user) - 1, -1, -1):
+    if user[i] == char_find:
+        index = i
+        break
 # print the output
+if index != -1:
+    print(f"The character '{char_find}' last appears at index {index}.")
+else:
+    print(f"The character '{char_find}' is not found in the string.")
