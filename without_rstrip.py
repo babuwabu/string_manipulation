354620659bd0946c9085d975e29c020d3452e7e9
Lou <marilounacional0717@gmail.com>
2025-04-01 16:18:13 +0800

    print

--- without_rstrip.py
+++ without_rstrip.py
@@ -1,3 +1,7 @@
 # ask user for input
+user = input("Enter your name: ")
 # manually remove trailing spaces 
+while len(user) > 0 and user[-1] == " ":
+    user = user[:-1]
 # print output
+print(repr(user))
