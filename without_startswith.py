49816c007fb244af174d31a7493b8c846cabfbab
Lou <marilounacional0717@gmail.com>
2025-04-01 16:48:58 +0800

    checks and print

--- without_startswith.py
+++ without_startswith.py
@@ -1,3 +1,7 @@
 # ask user for input
+user = input("Enter ypur name: ")
+prefix = input("Enter the prefix to check: ")
 # manually check if the string starts with the given prefix 
-# Print the result
+starts_with = len(user) >= len(prefix) and user[:len(prefix)] == prefix
+# print the output
+print(starts_with)
