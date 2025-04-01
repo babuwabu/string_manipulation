69f1d7408e9edd429508f6b004316f9635976900
Lou <marilounacional0717@gmail.com>
2025-04-01 16:27:58 +0800

    print all upper

--- without_upper.py
+++ without_upper.py
@@ -1,3 +1,12 @@
 # ask user for input
+user = input("Enter your name: ")
 # manually convert the input to uppercase
+upper_case = ""
+
+for char in user:
+    if "a" <= char <= "z":
+        upper_case += chr(ord(char) - 32)  # convert lowercase to uppercase
+    else:
+        upper_case += char  # unchanged char
 # print the output
+print(upper_case)
