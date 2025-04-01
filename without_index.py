6b8b28ff36a9d166e3131459c0cb0c24323c6c3f
Lou <marilounacional0717@gmail.com>
2025-04-01 17:21:17 +0800

    print the index of char

--- without_index.py
+++ without_index.py
@@ -1,3 +1,14 @@
 # ask user for input
+user = input("Enter your name: ")
+char_find = input("Enter the character to find: ")
 # find the first occurrence manually
+index = -1
+for i in range(len(user)):
+    if user[i] == char_find:
+        index = i
+        break
 # print the output
+if index != -1:
+    print(f"The character '{char_find}' first appears at index {index}.")
+else:
+    print(f"The character '{char_find}' is not found in the string.")
